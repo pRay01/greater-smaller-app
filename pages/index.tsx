1945bf87c3b44e006fd6c38501032d4ea07c675e
pRay01 <pray9563@gmail.com>
2022-11-18 12:41:10 +0530

    css added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,112 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [input, setInput] = useState<number>(0);
+  const [input2, setInput2] = useState<number>(0);
+  const [input3, setInput3] = useState<number>(0);
+  const [input4, setInput4] = useState<number>(0);
+  const [output, setOutput] = useState<number>(0);
+  const [output2, setOutput2] = useState<number>(0);
+
+  function Greater() {
+    let max = 0;
+    if (input > max) {
+      max = input;
+    }
+    if (input2 > max) {
+      max = input2;
+    }
+    if (input3 > max) {
+      max = input3;
+    }
+    if (input4 > max) {
+      max = input4;
+    }
+    setOutput(max);
+  }
+
+  function Smaller() {
+    let min = input;
+
+    if (input2 < min) {
+      min = input2;
+    }
+    if (input3 < min) {
+      min = input3;
+    }
+    if (input4 < min) {
+      min = input4;
+    }
+    setOutput2(min);
+  }
+
+  function Delete() {
+    setInput(0);
+    setInput2(0);
+    setInput3(0);
+    setInput4(0);
+    setOutput(0);
+    setOutput2(0);
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+    <div className="flex min-h-screen flex-col items-center bg-black justify-center py-2">
+      <div className=" space-x-4">
+        <input
+          type="number"
+          className="py-2 rounded-lg"
+          value={input}
+          placeholder="Type a number"
+          onChange={(e) => setInput(Number(e.target.value))}
+        />
+        <input
+          type="number"
+          className="py-2 rounded-lg"
+          value={input2}
+          placeholder="Type a number"
+          onChange={(e) => setInput2(Number(e.target.value))}
+        />
+        <input
+          type="number"
+          className="py-2 rounded-lg"
+          value={input3}
+          placeholder="Type a number"
+          onChange={(e) => setInput3(Number(e.target.value))}
+        />
+        <input
+          type="number"
+          className="py-2 rounded-lg"
+          value={input4}
+          placeholder="Type a number"
+          onChange={(e) => setInput4(Number(e.target.value))}
+        />
+        <div className="flex justify-center space-x-4 py-6 text-xl font-semibold">
+          <button
+            className="bg-green-400 px-6 py-2 rounded-lg"
+            onClick={Greater}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            Find Greatest
+          </button>
+          <button
+            className="bg-blue-400 px-6 py-2 rounded-lg"
+            onClick={Smaller}
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            Find Smallest
+          </button>
+          <button className="bg-red-500 px-6 py-2 rounded-lg" onClick={Delete}>
+            Clear
+          </button>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      </div>
+      <div className="flex space-x-10 text-white font-bold text-xl">
+        <div>Greatest: {output}</div>
+        <div>Samallest: {output2}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
